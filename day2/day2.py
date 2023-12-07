220a80a9db31bdb14a970582ff0885521ad124e5
Niels Rolleman <nielsrolleman@outlook.com>
2023-12-07 17:04:20 +0100

    progress

--- day2/day2.py
+++ day2/day2.py
@@ -1,4 +1,9 @@
 import time
+import re
+# red, green or blue cube
+# each line has a random number of each colored cube
+# which game would be possible if it only had a max of 12 red, 13 green and 14 blue
+# Get all the id's of which it's possible to play the game 
 
 # timer for functions
 def timer(func):
@@ -9,8 +14,22 @@ def timer(func):
     return t1-t0
 
 def part1():
+    arr = []
+    allNumbers = []
+
+    colors = ["red", "green", "blue"]
+    startOfGame = ":"
+    newSet = ","
+    endOfGame = ";"
     with open('./input.txt') as file:
         while line := file.readline():
             line.rstrip()
-            print(line)
+            allNumbers.append(re.findall(r'[\d]+', line))
+            arr.append(''.join(' '+x if 'A' <= 'Z' else x for x in line.replace(u'\xa0', '')).rstrip())
+            # arr.append(line)
+    file.close()
+
+    # for number in allNumbers:
+    #     print("id", number[0])
+    print(arr)
 part1()
